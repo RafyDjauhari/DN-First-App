4b1a1cdf9bc7919dd441c5d3453e90a0b0c09058
typeNull <rafydjauhari15@gmail.com>
2022-06-28 18:49:10 +0700

    Update 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
+import 'question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 // void main() => runApp(MyApp()); // this is the way of writing a main function for 1 expression only
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      if (_questionIndex == 1) {
+        _questionIndex = _questionIndex - 1;
+      } else {
+        _questionIndex = _questionIndex + 1;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite animal?",
+      "What's your favorite food?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,13 +42,38 @@ class MyApp extends StatelessWidget {
             child: Text("DN Service"),
           ),
         ),
-        body: Align(
-          child: ElevatedButton(
-            child: Text("asdasdas"),
-            onPressed: () {},
+        body: Column(children: [
+          Question(
+            questions[_questionIndex],
           ),
-          alignment: AlignmentDirectional.bottomCenter,
-        ),
+          ElevatedButton(
+            child: Text("Answer 1"),
+            onPressed: _answerQuestion,
+          ),
+          ElevatedButton(
+              child: Text("Answer 2"),
+              onPressed: () {
+                setState(() {
+                  if (_questionIndex == 1) {
+                    _questionIndex = _questionIndex - 1;
+                  } else {
+                    _questionIndex = _questionIndex + 1;
+                  }
+                });
+              }),
+          ElevatedButton(
+            child: Text("Answer 3"),
+            onPressed: () {
+              setState(() {
+                if (_questionIndex == 1) {
+                  _questionIndex = _questionIndex - 1;
+                } else {
+                  _questionIndex = _questionIndex + 1;
+                }
+              });
+            },
+          ),
+        ]),
       ),
     );
   }
